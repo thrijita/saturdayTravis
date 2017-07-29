cc5df8a1c6f5ab378599fedd974ebe30b39e9c5a
thrijita <kthrijita@gmail.com>
2017-07-29 12:51:45 +0530

    saturday

---
+++ src/main/java/com/stackroute/activitystream/IntegrationProjectTravis/Travis.java
@@ -0,0 +1,14 @@
+package com.stackroute.activitystream.IntegrationProjectTravis;
+
+public class Travis {
+	public static void main(String args[])
+	{
+		Travis obj=new Travis();
+		obj.sum(10, 20);
+	}
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+
+}
